ad66deef4fc47dc10344f13442f088936b81436f
okodoon <okdokd1031@gmail.com>
2019-09-16 22:37:33 +0900

    relationの微調整, db:seedが実行できるように修正

--- app/models/faculty.rb
+++ app/models/faculty.rb
@@ -1,6 +1,8 @@
 class Faculty 
   include Neo4j::ActiveNode
 
-
+  self.mapped_label_name = "学部"
+  property :name
+  has_many :in, :universities, type: :has, model_class: :University
 
 end
--- app/models/university.rb
+++ app/models/university.rb
@@ -1,6 +1,7 @@
 class University 
   include Neo4j::ActiveNode
 
-
-
+  self.mapped_label_name = "大学"
+  property :name
+  has_many :out, :faculties, type: :has, model_class: :Faculty
 end
